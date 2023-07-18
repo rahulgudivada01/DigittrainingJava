c09994dc7e0048b217c2c55c38be9cfcd5663d3e
rahulgudivada01 <139847466+rahulgudivada01@users.noreply.github.com>
2023-07-18 12:39:30 +0530

    Update TestingCode.java

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -3,9 +3,6 @@ package com.digit.testProj.p1;
 public class TestingCode {
 	public static void main(String[] args) {
 		System.out.println("Hi from Sachin");
-		System.out.println("Hi from Sachin");
-		System.out.println("Hi from Sachin");
-		System.out.println("Hi from Sachin");
 	}
 
 }
