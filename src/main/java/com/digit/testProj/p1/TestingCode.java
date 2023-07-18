85a09b6d0e46e0f404eaac4cbd0994a0d85edf2c
Rahul.Gudivada <Rahul.Gudivada@LDIGITBGL-L226.godigit.corp>
2023-07-18 12:42:57 +0530

    try

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -3,6 +3,7 @@ package com.digit.testProj.p1;
 public class TestingCode {
 	public static void main(String[] args) {
 		System.out.println("Hi from Sachin");
+		System.out.println("Hi from Dhoni");
 	}
 
 }
